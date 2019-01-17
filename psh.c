76c24557896305f7b31882367b144fe466c66c05
pepnou <pepnou1@gmail.com>
2019-01-17 11:05:34 +0100

    gestion de l'insertion en cours

--- psh.c
+++ psh.c
@@ -11,13 +11,135 @@
 #define DELLI "\033[K"
 
 
-void prompt()
+
+int prompt()
 {
 	char pr[] = "\nCC : ";
 	write(STDOUT_FILENO, pr, strlen(pr));
+
+	return strlen(pr) - 1; // -1 pour enlever la longueur de \n
 }
 
-int main(int argc, char** argv, char** envp)
+void handle( char c, char* buf, char* tmp, size_t* deb, size_t* cur, size_t* fin, size_t* size, int* over)
+{
+	if(c >= 32 && c <= 126)
+	{
+		if(*cur == *fin)
+		{
+			write(STDOUT_FILENO, &c, 1);
+			buf[*cur] = c;
+		}
+		else
+		{
+			//strncpy( &(buf[*cur + 1]), &(buf[*cur]), *fin - *cur);
+
+			strncpy( tmp, &(buf[*cur]), *fin - *cur);
+			strncpy( &(buf[*cur + 1]), tmp, *fin - *cur);
+
+
+			buf[*cur] = c;
+
+			write(STDOUT_FILENO, RESTC, strlen(RESTC));
+			write(STDOUT_FILENO, DELLI, strlen(DELLI));
+
+			write(STDOUT_FILENO, buf, *fin + 1);
+		}
+
+		(*cur)++;
+		(*fin)++;
+	}
+	else
+	{
+		switch(c)
+		{
+			case 4:
+			{
+				*over = 1;
+				write(STDOUT_FILENO, "\n", 1);
+				break;
+			}
+			case 10:
+			{
+				//write(STDOUT_FILENO, &c, 1);
+				prompt();
+
+				(*cur) = (*fin) = (*deb) = 0;
+				write(STDOUT_FILENO, SAVEC, strlen(SAVEC));
+				break;
+			}
+			case 21:
+			{
+				write(STDOUT_FILENO, RESTC, strlen(RESTC));
+				write(STDOUT_FILENO, DELLI, strlen(DELLI));
+				(*cur) = (*fin) = (*deb) = 0;
+				break;
+			}
+			case 127:
+			{
+				
+				break;
+			}
+			case 27:
+			{
+				read(STDIN_FILENO, &c, 1);
+
+				if(c == '[')
+				{
+					read(STDIN_FILENO, &c, 1);
+
+					switch(c)
+					{
+						case 'A':
+						{
+
+							break;
+						}
+						case 'B':
+						{
+
+							break;
+						}
+						case 'C':
+						{
+							if(*cur < *fin)
+							{
+								write(STDOUT_FILENO, FORWC, strlen(FORWC));
+								(*cur)++;
+							}
+							break;
+						}
+						case 'D':
+						{
+							if(*cur > 0)
+							{
+								write(STDOUT_FILENO, BACKC, strlen(BACKC));
+								(*cur)--;
+							}
+							break;
+						}
+						default:
+						{
+							handle(c, buf, tmp, deb, cur, fin, size, over);
+							break;
+						}
+					}
+				}
+				else
+				{
+					handle(c, buf, tmp, deb, cur, fin, size, over);
+				}
+
+				break;
+			}
+			default:
+			{
+				printf("%d\n", c);
+			}
+		}
+	}
+}
+
+int main( int argc, char** argv, char** envp)
 {
 	static struct termios old, new1;
 	int echo = 0;
@@ -30,10 +152,15 @@ int main(int argc, char** argv, char** envp)
     
 
 
-	char c, *buf;
-	size_t deb, cur, size = 128;
-	cur = deb = 0;
+	char c, *buf, *tmp;
+	size_t deb, cur, fin, size = 128;
+	cur = fin = deb = 0;
 	if(!(buf = malloc(size)))
+	{
+		perror("allocation du buffer : ");
+		exit(1);
+	}
+	if(!(tmp = malloc(size)))
 	{
 		perror("allocation du buffer : ");
 		exit(1);
@@ -43,46 +170,13 @@ int main(int argc, char** argv, char** envp)
 
 
 	prompt();
+	write(STDOUT_FILENO, SAVEC, strlen(SAVEC));
+
 	do
 	{
 		read(STDIN_FILENO, &c, 1);
 
-		if(c >= 32 && c <= 126)
-		{
-			write(STDOUT_FILENO, &c, 1);
-
-			buf[cur] = c;
-			cur++;
-		}
-		else
-		{
-			switch(c)
-			{
-				case 4:
-				{
-					over = 1;
-					break;
-				}
-				case 10:
-				{
-					//write(STDOUT_FILENO, &c, 1);
-					prompt();
-
-					cur = deb = 0;
-					write(STDOUT_FILENO, SAVEC, strlen(SAVEC));
-					break;
-				}
-				case 127:
-				{
-					write(STDOUT_FILENO, RESTC, strlen(RESTC));
-					break;
-				}
-				default:
-				{
-					printf("%d\n", c);
-				}
-			}
-		}
+		handle(c, buf, tmp, &deb, &cur, &fin, &size, &over);
 
 	} while(!over);
 
