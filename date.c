c6aff7911c89b1e8d4943f74db6e4d736e48321f
enderswype <enderswype@orange.fr>
2019-01-17 22:53:50 +0100

    date and intro, intro is pretty cool tho !

--- date.c
+++ date.c
@@ -10,7 +10,7 @@ int main(int argc, char const *argv[])
 
 	printf("Nous sommes le %d-%d-%d et il est %dh%dmin%dsec\n", now.tm_mday, now.tm_mon + 1, now.tm_year + 1900, now.tm_hour, now.tm_min, now.tm_sec);
 
-	printf("et\nil s'est écoulé %d secondes depuis le 1er janvier 1970 à minuit\n", temps);
-	
+	printf("\nET\n\nil s'est écoulé %d secondes depuis le 1er janvier 1970 à minuit\n", temps);
+
 	return 0;
 }
